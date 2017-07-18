6ec5f8eea4e3fef24ddd75508d5429c3422824f5
gordillo99 <metalj99@gmail.com>
2017-07-18 14:43:55 -0700

    1 cycle improvement

--- sqrt_v1_2.s
+++ sqrt_v1_2.s
@@ -6,7 +6,7 @@
 	.eabi_attribute 24, 1
 	.eabi_attribute 25, 1
 	.eabi_attribute 26, 2
-	.eabi_attribute 30, 6
+	.eabi_attribute 30, 2
 	.eabi_attribute 18, 4
 	.file	"sqrt_v1_2.c"
 	.text
@@ -15,109 +15,57 @@
 	.type	calculate_sqrt, %function
 calculate_sqrt:
 	@ Function supports interworking.
-	@ args = 0, pretend = 0, frame = 32
-	@ frame_needed = 1, uses_anonymous_args = 0
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
 	@ link register save eliminated.
-	str	fp, [sp, #-4]!
-	add	fp, sp, #0
-	sub	sp, sp, #36
-	str	r0, [fp, #-8]
-	str	r1, [fp, #-12]
-	ldr	r2, [fp, #-8]
 	mov	r3, #1
-	mov	r3, r3, asl r2
-	str	r3, [fp, #-28]
-	ldr	r2, [fp, #-8]
-	mov	r3, #1
-	mov	r3, r3, asl r2
-	str	r3, [fp, #-24]
-	ldr	r2, [fp, #-8]
-	ldr	r3, [fp, #-12]
-	mov	r3, r3, asl r2
-	str	r3, [fp, #-12]
-	mov	r0, #0
-	str	r0, [fp, #-32]
-	b	.L2
-.L4:
-	ldr	r3, [fp, #-28]
-	mov	r2, r3, asl #2
-	ldr	r3, [fp, #-32]
-	mov	r3, r2, lsr r3
-	ldr	r0, [fp, #-28]
-	add	r1, r3, r0
-	ldr	r3, [fp, #-28]
-	mov	r2, r3, asl #2
-	ldr	r0, [fp, #-32]
-	mov	r3, r0, asl #1
-	mov	r3, r2, lsr r3
-	add	r1, r1, r3
-	str	r1, [fp, #-20]
-	ldr	r2, [fp, #-32]
-	sub	r3, r2, #1
-	ldr	r0, [fp, #-24]
-	mov	r3, r0, lsr r3
-	ldr	r2, [fp, #-24]
-	add	r3, r3, r2
-	str	r3, [fp, #-16]
-	ldr	r3, [fp, #-12]
-	ldr	r0, [fp, #-20]
-	cmp	r0, r3
-	bhi	.L3
-	ldr	r2, [fp, #-20]
-	str	r2, [fp, #-28]
-	ldr	r3, [fp, #-16]
-	str	r3, [fp, #-24]
+	stmfd	sp!, {r4, r5, r6}
+	mov	r4, r3, asl r0
+	subs	r6, r0, #1
+	mov	r5, r1, asl r0
+	moveq	r0, r4
+	beq	.L3
+	mov	r0, r4
+	mov	ip, #0
+.L8:
+	mov	r3, ip, asl #1
+	add	r3, r4, r4, lsr r3
+	mov	r2, r4, asl #1
+	add	r3, r3, r2, lsr ip
+	add	r1, r0, r0, lsr ip
+	cmp	r5, r3
+	add	ip, ip, #1
+	movcs	r4, r3
+	movcs	r0, r1
+	cmp	ip, r6
+	bne	.L8
 .L3:
-	ldr	r0, [fp, #-32]
-	add	r0, r0, #1
-	str	r0, [fp, #-32]
-.L2:
-	ldr	r3, [fp, #-8]
-	sub	r3, r3, #1
-	ldr	r2, [fp, #-32]
-	cmp	r3, r2
-	bne	.L4
-	ldr	r3, [fp, #-24]
-	mov	r0, r3
-	add	sp, fp, #0
-	ldmfd	sp!, {fp}
+	ldmfd	sp!, {r4, r5, r6}
 	bx	lr
 	.size	calculate_sqrt, .-calculate_sqrt
-	.section	.rodata
-	.align	2
-.LC0:
-	.ascii	"%d\012\000"
-	.text
 	.align	2
 	.global	main
 	.type	main, %function
 main:
 	@ Function supports interworking.
-	@ args = 0, pretend = 0, frame = 8
-	@ frame_needed = 1, uses_anonymous_args = 0
-	stmfd	sp!, {fp, lr}
-	add	fp, sp, #4
-	sub	sp, sp, #8
-	mov	r3, #14
-	str	r3, [fp, #-12]
-	mov	r3, #2
-	str	r3, [fp, #-8]
-	ldr	r0, [fp, #-12]
-	ldr	r1, [fp, #-8]
-	bl	calculate_sqrt
-	mov	r3, r0
-	ldr	r0, .L8
-	mov	r1, r3
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
+	mov	r1, #23040
+	stmfd	sp!, {r4, lr}
+	add	r1, r1, #128
+	ldr	r0, .L14
 	bl	printf
-	mov	r3, #0
-	mov	r0, r3
-	sub	sp, fp, #4
-	ldmfd	sp!, {fp, lr}
+	mov	r0, #0
+	ldmfd	sp!, {r4, lr}
 	bx	lr
-.L9:
+.L15:
 	.align	2
-.L8:
+.L14:
 	.word	.LC0
 	.size	main, .-main
+	.section	.rodata.str1.4,"aMS",%progbits,1
+	.align	2
+.LC0:
+	.ascii	"%d\012\000"
 	.ident	"GCC: (Sourcery G++ Lite 2008q3-72) 4.3.2"
 	.section	.note.GNU-stack,"",%progbits
