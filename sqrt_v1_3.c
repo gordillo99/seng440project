dffdc1b5a76abca9e2de573d1571fca0f3b804b6
Cyrus Erfani <cyrus.erfani@gmail.com>
2017-07-22 16:01:01 -0700

    all c files now updated and working

--- sqrt_v1_3.c
+++ sqrt_v1_3.c
@@ -11,7 +11,6 @@ uint32_t calculate_sqrt(uint32_t K, uint32_t M) {
 	register uint32_t f_sqrt = f;
 	register uint32_t MU;
 	register uint32_t small_k = K - 1;
-	M = M << K; // final output = sqrt(M)
 
 	for (i ^= i; i <= small_k; i += 2) { // have to use <= for loop unrolling
 		MU = f + ( (f << 1) >> i ) + ( f >> (i << 1) );    
@@ -33,10 +32,10 @@ uint32_t calculate_sqrt(uint32_t K, uint32_t M) {
 }
 
 int main() {
-	uint32_t K = 14; // precision bits
-  uint32_t M = 2; // sqrt(M)
-	
-	printf("%d\n", calculate_sqrt(K, M));
+	uint32_t K = 29; // precision bits
+  	uint32_t M = (unsigned int) (2.0 * (1 << K)); // sqrt(M)
+	M = calculate_sqrt(K, M);
+	printf("%f\n", (float) M/(1 << K));
 	return 0;
 }
 
