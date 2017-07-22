fcb3504608e39b27c8aeb6a117948504a9da8ff7
Jose Gordillo <gordillo@uvic.ca>
2017-07-22 13:43:58 -0700

    fixing another mistake

--- sqrt_v1_2.c
+++ sqrt_v1_2.c
@@ -12,8 +12,10 @@ uint32_t calculate_sqrt(uint32_t K, uint32_t M) {
 	register uint32_t f = 1 << K;
 	register uint32_t f_sqrt = f;
 	register uint32_t MU;
+    register uint32_t small_k = K - 1;
 
-	for (i ^= i; i != K - 1; i++) {
+	for (i ^= i; i != small_k; i++) {
+
 		MU = f + ( (f << 1) >> i ) + ( f >> (i << 1) ); // previously (f * (a * a >> K)) >> K;      
 		
 		if (MU <= M) {
